290e4d91f14e43e6418cdfb1f6ddfd715197e705
pratik-jetatech <pratik@jetatech.in>
2024-11-23 19:49:52 +0530

    changed test class name

--- src/test/java/com/dws/challenge/TransferAmountFunctionalityTest.java
+++ src/test/java/com/dws/challenge/TransferAmountFunctionalityTest.java
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
-public class TransferAmountTest {
+public class TransferAmountFunctionalityTest {
 
     @InjectMocks
     private AccountsRepositoryInMemory repository;
